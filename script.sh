c8bb06d86e5d4d7319b174909a2e0c9d3bb2e139
Deobrat QMatX <137123400+deobratsingh9@users.noreply.github.com>
2026-02-22 21:29:52 +0100

    Add files via upload
    Overview
    
    COHP (Crystal Orbital Hamilton Population) and
    COOP (Crystal Orbital Overlap Population) are bonding analysis tools used to:
    
    Identify bonding / antibonding interactions
    
    Quantify bond strength (ICOHP)
    
    Analyze orbital contributions to bonding
    
    Evaluate structural stability
    
    COHP is generally preferred over COOP due to its stronger physical interpretation.
    
    🧠 Theory Background
    COOP
    
    Based on overlap population
    
    Positive → Bonding
    
    Negative → Antibonding
    
    COHP
    
    Based on Hamiltonian matrix elements
    
    Negative COHP → Bonding
    
    Positive COHP → Antibonding
    
    Often plotted as –COHP so bonding appears positive
    
    ICOHP
    
    Integrated COHP value:
    
    More negative → Stronger bond

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J 3D-BN
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=8
+#SBATCH -t 000:20:00
+#SBATCH -A naiss2025-5-112
+
+module add VASP/5.4.4.16052018-nsc1-intel-2018a-eb
+mpprun --compat el7 vasp_std
