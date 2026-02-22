54af98e60c60e5e66abbc7864eb314b6f46a6200
Deobrat QMatX <137123400+deobratsingh9@users.noreply.github.com>
2026-02-22 21:33:01 +0100

    Change SLURM account allocation in script.sh
    Updated the account allocation for SLURM job.

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=8
 #SBATCH -t 000:20:00
-#SBATCH -A naiss2025-5-112
+#SBATCH -A xxx
 
 module add VASP/5.4.4.16052018-nsc1-intel-2018a-eb
 mpprun --compat el7 vasp_std
